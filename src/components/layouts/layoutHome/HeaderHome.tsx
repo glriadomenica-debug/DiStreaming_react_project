08ec3d2bd4383a02f0c360a603353879d13ed0e1
glriadomenica-debug <glriadomenica@gmail.com>
2026-05-05 00:49:54 +0700

    update header

--- src/components/layouts/layoutHome/HeaderHome.tsx
+++ src/components/layouts/layoutHome/HeaderHome.tsx
@@ -1,4 +1,10 @@
 import { useNavigate } from "react-router-dom";
+import { IoMdPersonAdd } from "react-icons/io";
+import { IoIosListBox } from "react-icons/io";
+import { TfiControlPlay } from "react-icons/tfi";
+import { IoMdPlay } from "react-icons/io";
+import { CgLogOff } from "react-icons/cg";
+
 
 export default function HeaderHome() {
   const navigate = useNavigate();
@@ -14,21 +20,24 @@ export default function HeaderHome() {
       {/* Header */}
       <header className="bg-[url('https://static.vecteezy.com/system/resources/thumbnails/004/717/032/small/scary-dark-red-walls-slightly-light-dark-concrete-cement-texture-for-background-photo.jpg')]">
         <div className="max-w-7xl mx-auto px-4 flex flex-wrap justify-between py-3 items-center">
-          <a href="/genre" className="text-lg md:text-xl text-white font-bold ">Di Streaming</a>
+          <a href="/genre" className="flex items-center gap-2 text-lg md:text-xl text-white font-bold ">  <IoMdPlay /> Di Streaming</a>
           <div className="flex items-center gap-4 mt-2 md:mt-0">
-            <a href="/genre" className="text-white hover:underline">
+            <a href="/genre" className="flex items-center gap-2 text-white hover:underline">
+              <TfiControlPlay />
               Movie Genre
             </a>
-            <a href="/category" className="text-white hover:underline">
+            <a href="/category" className="flex items-center gap-2 text-white hover:underline">
+              <IoIosListBox />
               Category
             </a>
-            <a href="/user" className="text-white hover:underline">
+            <a href="/user" className="flex items-center gap-2 text-white hover:underline">
+              <IoMdPersonAdd />
               User
             </a>
             <a
               onClick={handleLogout}
-              className="bg-black/30 hover:bg-black text-white font-semibold px-3 py-1 rounded cursor-pointer"
-            >
+              className="flex items-center gap-2 bg-black/30 hover:bg-black text-white font-semibold px-3 py-1 rounded cursor-pointer"
+            ><CgLogOff />
               Log out
             </a>
           </div>
--- src/components/layouts/layoutLogin/HeaderLogin.tsx
+++ src/components/layouts/layoutLogin/HeaderLogin.tsx
@@ -1,3 +1,7 @@
+import { IoMdPlay } from "react-icons/io";
+import { SiSimplelogin } from "react-icons/si";
+import { CgLogIn } from "react-icons/cg";
+
 
 export default function HeaderLogin() {
 
@@ -6,10 +10,12 @@ export default function HeaderLogin() {
       {/* Header */}
       <header className="bg-cover bg-center bg-[url('https://static.vecteezy.com/system/resources/thumbnails/004/717/032/small/scary-dark-red-walls-slightly-light-dark-concrete-cement-texture-for-background-photo.jpg')]">
         <div className="px-4 md:px-8 lg:px-12 flex justify-between h-14 items-center">
-          <div className="text-xl text-white w-1/4 ">Di Streaming
+          <div className="flex items-center gap-2 text-xl text-white w-1/4 "> <IoMdPlay />
+            Di Streaming
           </div>
           <div className="flex items-center gap-2 md:gap-4 ">
-            <a href="/login" className="text-white text-sm md:text-base font-semibold px-3 py-1 md:px-4 md:py-2 rounded hover:bg-black/70 transition">
+            <a href="/login" className="flex items-center gap-2 text-white text-sm md:text-base font-semibold px-3 py-1 md:px-4 md:py-2 rounded hover:bg-black/70 transition">
+              <CgLogIn />
               Login
             </a>
             {/* <a href="/register" className="hover:bg-black text-white font-bold py-2 px-4 rounded transition-all">
